ec9a110576e3ed433b462d206018e68d9056b70f
Denis <deniskalpeshbhai436@gmail.com>
2024-05-12 23:56:05 +0530

    second commit

---
+++ 2_input_output.py
@@ -0,0 +1,17 @@
+# Taking input from the user
+str = input("Enter a string: ")
+
+# Output
+print("The string is: ", str)
+
+
+
+
+
+# Taking integer input from the user
+num = int(input("Enter a value: "))
+
+add = num + 5
+
+# Output
+print("The sum is %d" %add)
